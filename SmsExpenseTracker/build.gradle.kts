e4de40d1b4ee7f93df3d00e8cdaec6b5b87ff31c
sankarmahadevan <kssankarmahadevan@gmail.com>
2026-09-16 20:36:46 +0530

    Update build.gradle.kts

--- SmsExpenseTracker/build.gradle.kts
+++ SmsExpenseTracker/build.gradle.kts
@@ -1,7 +1,6 @@
-// Top-level build file
 plugins {
-    id("com.android.application") version "9.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
 }
